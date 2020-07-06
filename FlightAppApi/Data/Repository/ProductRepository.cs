a0e16ea9cc84ef488da7d0335f023753298931ca
SWojtyla <sebastien.wojtyla.y3274@student.hogent.be>
2020-07-06 16:55:40 +0200

    Add ProductRepository

---
+++ FlightAppApi/Data/Repository/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using FlightAppApi.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightAppApi.Data.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly FlightDbContext _context;
+        private readonly DbSet<Product> _products;
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _products;
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return _products.Where(p => p.Category == category);
+        }
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ FlightAppApi/Model/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightAppApi.Model
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Category { get; set; }
+        public Bitmap Image { get; set; }
+    }
+}
